b65f71f00c428fdc386497656ef876223e8b1294
marcossobralramos <34129988+marcossobralramos@users.noreply.github.com>
2018-10-17 20:57:07 -0300

    Add comentários.

--- static/js/pong.js
+++ static/js/pong.js
@@ -29,7 +29,7 @@ socket.on('get players points', (data) => {
   playerLeft = (numPlayer === 1) ? points.me : points.adv;
   playerRight = (numPlayer === 2) ? points.me : points.adv;
 
-  // create text for the score, set font properties
+  // cria texto para o score e define as propriedades da fonte
   scoreLeft = draw.text(playerLeft+'').font({
     size: 32,
     family: 'Menlo, sans-serif',
@@ -37,7 +37,7 @@ socket.on('get players points', (data) => {
     fill: '#fff'
   }).move(width/2-10, 10);
 
-  // cloning rocks!
+  // clonando
   scoreRight = scoreLeft.clone()
     .text(playerRight+'')
     .font('anchor', 'start')
@@ -45,70 +45,63 @@ socket.on('get players points', (data) => {
 
 });
 
-// define document width and height
+// define o tamanho da mesa do ping-pong
 var width = 450, height = 300
 
-// create SVG document and set its size
+// Cria o documento SVG setando o tamanho
 var draw = SVG('pong').size(width, height)
 draw.viewbox(0,0,450,300)
 
-// draw background
+// desenha o background
 var background = draw.rect(width, height).fill('#dde3e1')
 
-// draw line
+// desenha linha
 var line = draw.line(width/2, 0, width/2, height)
 line.stroke({ width: 5, color: '#fff', dasharray: '5,5' })
 
 
-// define paddle width and height
+// define o tamanho da barra dos jogadores
 var paddleWidth = 15, paddleHeight = 80
 
-// create and position left paddle
+// cria e posiciona a barra do jogador à esquerda
 var paddleLeft = draw.rect(paddleWidth, paddleHeight)
 paddleLeft.x(0).cy(height/2).fill('#00ff99')
 
-// create and position right paddle
+// cria e posiciona a barra do jogador à direita
 var paddleRight = paddleLeft.clone()
 paddleRight.x(width-paddleWidth).fill('#ff0066')
 
 
-// define ball size
+// define o tamanho da bola
 var ballSize = 10
 
-// create ball
+// cria a bola
 var ball = draw.circle(ballSize)
 ball.center(width/2, height/2).fill('#7f7f7f')
 
-// random velocity for the ball at start
+// velocidade da bola
 var vx = 0, vy = 0
 
-// AI difficulty
-var difficulty = 2
-
-// update is called on every animation step
+// atualiza o movimento da bola
 function update(dt) {
   socket.emit('move ball', dt);
 }
 
 socket.on('move ball', function(dt){
-  // move the ball by its velocity
+  // move a bola de acordo com a velocidade
   ball.dmove(vx*dt, vy*dt)
 
-  // get position of ball
+  // pega a posição da bola
   var cx = ball.cx()
     , cy = ball.cy()
 
-  // get position of ball and paddle
+  // pega a posição da bola e da barra esquerda
   var paddleLeftCy = paddleLeft.cy()
 
-  // move the left paddle in the direction of the ball
-  /*var dy = Math.min(difficulty, Math.abs(cy - paddleLeftCy))
-  paddleLeftCy += cy > paddleLeftCy ? dy : -dy*/
-
-  // constraint the move to the canvas area
+  // restringe o movimento para a área da lona
   paddleLeft.cy(Math.max(paddleHeight/2, Math.min(height-paddleHeight/2, paddleLeftCy)))
 
-  // check if we hit top/bottom borders
+  // verifica se a bola atingiu a margem inferior/superior
   if ((vy < 0 && cy <= 0) || (vy > 0 && cy >= height)) {
     vy = -vy
   }
@@ -116,19 +109,17 @@ socket.on('move ball', function(dt){
   var paddleLeftY = paddleLeft.y()
     , paddleRightY = paddleRight.y()
 
-  // check if we hit the paddle
+  // verifica se a barra foi atingida
   if((vx < 0 && cx <= paddleWidth && cy > paddleLeftY && cy < paddleLeftY + paddleHeight) ||
     (vx > 0 && cx >= width - paddleWidth && cy > paddleRightY && cy < paddleRightY + paddleHeight)) {
-    // depending on where the ball hit we adjust y velocity
-    // for more realistic control we would need a bit more math here
-    // just keep it simple
-    vy = (cy - ((vx < 0 ? paddleLeftY : paddleRightY) + paddleHeight/2)) * 7 // magic factor
+    // dependendo onde a bola bate, ajusta-se a velocidade y
+    vy = (cy - ((vx < 0 ? paddleLeftY : paddleRightY) + paddleHeight/2)) * 7
 
-    // make the ball faster on hit
+    // torna bola mais rápida ao acertar
     vx = -vx * 1.05
   } else
 
-  // check if we hit left/right borders
+  // verifica as bordas laterias foram atingidas (se foi feito ponto)
   if ((vx < 0 && cx <= 0) || (vx > 0 && cx >= width)) {
     // O player 2 informará para o servidor que marcou ponto, da mesma for fará o player 1 quando marcar
     if(vx < 0 && numPlayer === 2) 
@@ -159,7 +150,7 @@ socket.on('move ball', function(dt){
     socket.emit('get players victories', null);
   });
 
-  // move player right paddle
+  // move o jogador da barra à direita
   var playerRightPaddleY = paddleRight.y()
 
   if (playerRightPaddleY <= 0 && paddleRightDirection == -1) {
@@ -170,7 +161,7 @@ socket.on('move ball', function(dt){
     paddleRight.dy(paddleRightDirection*paddleSpeed)
   }
 
-  // move player left paddle
+  // move o jogador da barra à esquerda
   var playerLeftPaddleY = paddleLeft.y()
 
   if (playerLeftPaddleY <= 0 && paddleLeftDirection == -1) {
@@ -181,17 +172,17 @@ socket.on('move ball', function(dt){
     paddleLeft.dy(paddleLeftDirection*paddleSpeed)
   }
 
-  // update ball color based on position
+  // atualiza a cor da bola baseando na posição
   ball.fill(ballColor.at(1/width*ball.x()))
 });
 
 var lastTime, animFrame
 
 function callback(ms) {
-  // we get passed a timestamp in milliseconds
-  // we use it to determine how much time has passed since the last call
+  // recebe-se um timestamp em milissegundos
+  // usa-se para determinar quanto tempo passou desde a última chamada
   if (lastTime) {
-    update((ms-lastTime)/1000) // call update and pass delta time in seconds
+    update((ms-lastTime)/1000) // chama a atualização e passa o tempo em segundos
   }
 
   lastTime = ms
@@ -238,41 +229,38 @@ draw.on('click', function() {
 })
 
 function reset() {
-  // visualize boom
+  // chamada da animação boom
   boom()
   
-  // reset speed values
+  // reseta a velocidade
   vx = 0
   vy = 0
 
-  // position the ball back in the middle
+  // posiciona a bola no meio da mesa
   ball.animate(100).center(width/2, height/2)
 
-  // reset the position of the paddles
+  // reseta a posição das barras
   paddleLeft.animate(100).cy(height/2)
   paddleRight.animate(100).cy(height/2)
 }
 
-// ball color update
+// reseta a cor da bola
 var ballColor = new SVG.Color('#ff0066')
 ballColor.morph('#00ff99')
 
-// show visual explosion 
+// animação boom
 function boom() {
-  // detect winning player
+
   var paddle = ball.cx() > width/2 ? paddleLeft : paddleRight
 
-  // create the gradient
   var gradient = draw.gradient('radial', function(stop) {
     stop.at(0, paddle.attr('fill'), 1)
     stop.at(1, paddle.attr('fill'), 0)
   })
 
-  // create circle to carry the gradient
   var blast = draw.circle(300)
   blast.center(ball.cx(), ball.cy()).fill(gradient)
 
-  // animate to invisibility
   blast.animate(1000, '>').opacity(0).after(function() {
     blast.remove()
   });
